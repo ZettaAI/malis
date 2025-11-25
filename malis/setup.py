2600f65e556677b4a4ae694cdd21c78328809990
Kisuk Lee <kisuk@zetta.ai>
2025-11-25 17:51:03 +0900

    build: modernize malis build for Python 3.10 and PEP 517 - Replace deprecated distutils usage with setuptools and sysconfig to ensure compatibility with Python 3.10+. - Update make.sh to build using pip install -e . --use-pep517 instead of invoking setup.py directly. - Refactor setup.py to use sysconfig for resolving include and library paths, replacing removed distutils.sysconfig calls. - Define explicit version constraints for build dependencies (cython>=0.24, numpy>=1.9) and specify python_requires='>=3.6'. - Clean up C++ extension compilation flags and formatting in malis/setup.py.

--- malis/setup.py
+++ malis/setup.py
@@ -1,13 +1,23 @@
 def setup_cython():
-	from distutils.core import setup
-	from distutils.extension import Extension
-	from Cython.Distutils import build_ext
+    from setuptools import setup, Extension
+    from Cython.Build import cythonize
+    import numpy
 
-	import numpy
+    ext_modules = [
+        Extension(
+            "malis",
+            ["malis.pyx", "malis_cpp.cpp"],
+            language='c++',
+            extra_link_args=["-std=c++11"],
+            extra_compile_args=["-std=c++11", "-w"]
+        )
+    ]
 
-	ext_modules = [Extension("malis", ["malis.pyx", "malis_cpp.cpp"], language='c++',extra_link_args=["-std=c++11"],
-                         extra_compile_args=["-std=c++11", "-w"])]
+    setup(
+        ext_modules=cythonize(ext_modules),
+        include_dirs=[numpy.get_include()]
+    )
 
-	setup(cmdclass = {'build_ext': build_ext}, include_dirs=[numpy.get_include()], ext_modules = ext_modules)
-if __name__=='__main__':
-	setup_cython()
+
+if __name__ == '__main__':
+    setup_cython()
